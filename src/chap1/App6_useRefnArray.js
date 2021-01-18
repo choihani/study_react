1bb07107e33dd6775529c95e18428eed16419295
choihani <hani_haha@naver.com>
2021-01-18 17:50:37 +0900

    chore(react): add react directory
    velopert chapter13~17

--- src/chap1/App6_useRefnArray.js
+++ src/chap1/App6_useRefnArray.js
@@ -1,33 +1,96 @@
-import React,{useRef} from 'react';
+import React,{useRef, useState, useMemo} from 'react';
 import UserList2 from './UserList2';
+import CreateUser from './CreateUser';
 
-function App() {
-  const users = [
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+function App6() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
-      //나중에 구현 할 배열에 항목 추가하는 로직
-      // ...
-
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers([...users, user]);
+      //setUsers(users.concat(user));
+      
+      //imput의 값 초기화
+      setInputs({
+        username: '',
+        email: ''
+      });
       nextId.current += 1;
   }
-  return <UserList2 users={users} />;
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+  // const count = countActiveUsers(users);
+  /*
+  useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고
+   두번째 파라미터에는 deps 배열을 넣어주면 되는데, 
+  이 배열 안에 넣은 내용이 바뀌면, 우리가 등록한 함수를 호출해서 값을 연산해주고, 
+  만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.
+  */
+  const count = useMemo(() => countActiveUsers(users),[users]);
+  return (
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList2 users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
+    </>
+  );
 }
 
-export default App;
+export default App6;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App5 from './chap1/App5_Array';
+import App6 from './chap1/App6_useRefnArray';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render( //브라우저에 있는 실제 DOM 내부에 리액크 컴포넌트를 렌더링하겠다는 것
   <React.StrictMode>
-    <App5 />
+    <App6 />
   </React.StrictMode>,
   document.getElementById('root') //리액트 컴포넌트가 렌더링 될 때에는, 렌더링된 결과물이 위 div 내부에 렌더링되는 것
 );
